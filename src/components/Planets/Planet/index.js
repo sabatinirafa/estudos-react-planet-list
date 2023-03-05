60d9b179cf7610a6a2a598743bb6056222e1c03c
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-03-04 22:02:07 -0300

    refatorando-planet-com-hooks

--- src/components/Planets/Planet/index.js
+++ src/components/Planets/Planet/index.js
@@ -1,6 +1,6 @@
 import GreyImg from '../../shared/grey-img'
 import Description from '../../shared/description'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 let getSatellites = async function (id) {
   let response = await fetch(`http://localhost:3000/api/${id}.json`)
@@ -8,45 +8,43 @@ let getSatellites = async function (id) {
   return data
 }
 
-class Planet extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      satellites: []
-    }
-  }
+// componentDidMount() {
+//   getSatellites(this.props.id).then(data => {
+//     this.setState({
+//       satellites: data['satellites']
+//     })
+//   })
+// }
 
-  componentDidMount() {
-    getSatellites(this.props.id).then(data => {
-      this.setState({
-        satellites: data['satellites']
-      })
+
+const Planet = (props) => {
+  const [satellites, setSatellites] = useState([])
+
+  useEffect(() => {
+    getSatellites(props.id).then(data => {
+      setSatellites(data['satellites'])
     })
-  }
+  },[])
 
-  render() {
-    return (
-      <div>
-        <h4>{this.props.name}</h4>
-        <Description
-          description={this.props.description}
-          link={this.props.link}
-        />
-        <GreyImg 
-          img_url={this.props.img_url}
-        />
-        <h4>Luas do satélite</h4>
-        <ul>
-          {this.state.satellites.map((satellite, index) => 
-            <li key={index}>{satellite.name}</li>
-          )}
-        </ul>
-        <hr/>
-      </div>
-    )
-  }
-
-
+  return (
+    <div>
+      <h4>{props.name}</h4>
+      <Description
+        description={props.description}
+        link={props.link}
+      />
+      <GreyImg 
+        img_url={props.img_url}
+      />
+      <h4>Luas do satélite</h4>
+      <ul>
+        {satellites.map((satellite, index) => 
+          <li key={index}>{satellite.name}</li>
+        )}
+      </ul>
+      <hr/>
+    </div>
+  )
 }
 
 
